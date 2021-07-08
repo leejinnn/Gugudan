bd90ce5b7eeefa7f0987367b45ff339582da1f9e
82102 <82102@DESKTOP-4RQDRGI>
2021-07-08 10:26:08 +0900

    initial origin

---
+++ src/gugudan2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class gugudan2 {
+ public static void main(String[] args) {
+	 System.out.println("구구단 중 출력할 단은:");
+	 Scanner scanner=new Scanner(System.in);
+	 int number=scanner.nextInt();
+	 System.out.println("number:"+number);
+	 
+	 if (number<2) {
+		 System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+	 }else if(number>9){
+		 System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+	 }else {
+		 
+		for(int i=1;i<9;i++) {
+			System.out.println(number*i);
+		}
+		 
+	 }
+	 
+	 
+		 
+ }
+}
